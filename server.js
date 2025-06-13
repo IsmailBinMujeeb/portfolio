4a67ebb2e61fa1b2c0e30b5b84424fcf296069c1
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-06-13 17:03:06 +0530

    template setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import env from "./config/env.js";
 
+import indexRouter from "./routes/index.route.js";
+
 const app = express();
 const PORT = env.PORT || 3000;
 
@@ -10,5 +12,7 @@ app.set("port", PORT)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use("/", indexRouter);
 
 export default app;
